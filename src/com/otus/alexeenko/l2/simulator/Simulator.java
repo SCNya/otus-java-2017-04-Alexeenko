eacbb4eedb91e4aa1b12541df5a3a5374c0f5280
Vsevolod <alexeenko.vsevolod@gmail.com>
2017-04-09 16:41:14 +0300

    Beta version 4

--- src/com/otus/alexeenko/l2/simulator/Simulator.java
+++ src/com/otus/alexeenko/l2/simulator/Simulator.java
@@ -1,6 +1,7 @@
 package com.otus.alexeenko.l2.simulator;
 
 import java.math.BigInteger;
+import java.util.function.Supplier;
 
 /**
  * Created by Vsevolod on 08/04/2017.
@@ -13,51 +14,84 @@ public class Simulator {
     private long memoryBeforeTest;
     private BigInteger memoryConsumption;
     private Object[] store;
+    private Supplier<Object> getItem;
 
     public Simulator() {
         runtime = Runtime.getRuntime();
-        memoryConsumption = new BigInteger("0");
     }
 
     public long getSize(Class classType, Class parameterType, Object obj) {
 
+        if (classType != String.class || parameterType != String.class || obj.getClass() != String.class) {
+            getItem = new Supplier<Object>() {
+                Class cType = classType;
+                Class pType = parameterType;
+                Object object = obj;
+
+                @Override
+                public Object get() {
+                    try {
+                        return cType.getConstructor(pType).newInstance(object); //initialize store by obj
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                        return null;
+                    }
+                }
+            };
+        }
+        else {
+            getItem = new Supplier<Object>() {
+                Class cType = classType;
+                Class pType = parameterType;
+                Object object = obj;
+
+                @Override
+                public Object get() {
+                    try {
+                        return cType.getConstructor(pType).newInstance("".concat(((String) object)));
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                        return null;
+                    }
+                }
+            };
+
+        }
+
         System.out.println("\nSimulating... \nObj Type: " + classType + "\nConstructor Type: "
                 + parameterType);
 
-        memoryConsumption = BigInteger.valueOf(0);
-
-        for (int i = 0; i < numberOfTests; i++) {
-            //System.out.println("Test: " + i);
-            test(classType, parameterType, obj);
-            clean();
-        }
-        return memoryConsumption.divide(BigInteger.valueOf(numberOfTests)).longValue() / numberOfItems;
-    }
-
-    public long getSize(Class<String> classType, Class<String> parameterType, String obj) {
-
-        System.out.println("\nSimulating... \nObj Type: " + classType + "\nConstructor Type: "
-                + parameterType);
-
-        memoryConsumption = BigInteger.valueOf(0);
-
-        for (int i = 0; i < numberOfTests; i++) {
-            //System.out.println("Test: " + i);
-            test(classType, parameterType, obj);
-            clean();
-        }
-        return memoryConsumption.divide(BigInteger.valueOf(numberOfTests)).longValue() / numberOfItems;
+        return simulate();
     }
 
     public long getSize(Class classType) {
 
+        getItem = new Supplier<Object>() {
+            Class cType = classType;
+
+            @Override
+            public Object get() {
+                try {
+                    return cType.newInstance();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    return null;
+                }
+            }
+        };
+
+
         System.out.println("\nSimulating... \nObj Type: " + classType + "\nConstructor Type: Default");
+
+        return simulate();
+    }
+
+    private long simulate() {
 
         memoryConsumption = BigInteger.valueOf(0);
 
         for (int i = 0; i < numberOfTests; i++) {
-            //System.out.println("Test: " + i);
-            test(classType);
+            test();
             clean();
         }
         return memoryConsumption.divide(BigInteger.valueOf(numberOfTests)).longValue() / numberOfItems;
@@ -71,62 +105,14 @@ public class Simulator {
     }
 
     //@SuppressWarnings("unchecked")
-    private void test(Class classType, Class parameterType, Object obj) {
+    private void test() {
         System.gc();
         memoryBeforeTest = runtime.freeMemory();
 
         this.store = new Object[numberOfItems];
 
         for (int i = 0; i < numberOfItems; i++) {
-            try {
-                store[i] = classType.getConstructor(parameterType).newInstance(obj); //initialize store by obj
-                //store[i] = classType.getConstructor(parameterType).newInstance(new String("".concat(((String) obj)))); //initialize store by obj
-            }
-            catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-
-        System.gc();
-
-        memoryConsumption = memoryConsumption.add(BigInteger.valueOf(memoryBeforeTest - runtime.freeMemory()));
-    }
-
-    //@SuppressWarnings("unchecked")
-    private void test(Class<String> classType, Class<String> parameterType, String obj) {
-        System.gc();
-        memoryBeforeTest = runtime.freeMemory();
-
-        this.store = new Object[numberOfItems];
-
-        for (int i = 0; i < numberOfItems; i++) {
-            try {
-                store[i] = classType.getConstructor(parameterType).newInstance(new String("".concat(((String) obj)))); //initialize store by obj
-            }
-            catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-
-        System.gc();
-
-        memoryConsumption = memoryConsumption.add(BigInteger.valueOf(memoryBeforeTest - runtime.freeMemory()));
-    }
-
-    //@SuppressWarnings("unchecked")
-    private void test(Class classType) {
-        System.gc();
-        memoryBeforeTest = runtime.freeMemory();
-
-        this.store = new Object[numberOfItems];
-
-        for (int i = 0; i < numberOfItems; i++) {
-            try {
-                store[i] = classType.newInstance(); //initialize by default constructor
-            }
-            catch (Exception e) {
-                e.printStackTrace();
-            }
+                store[i] = getItem.get(); //initialize store
         }
 
         System.gc();
