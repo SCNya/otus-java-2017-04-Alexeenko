d9b114f17e59471ff21ac41aea3e1af2e5d49f88
Vsevolod <alexeenko.vsevolod@gmail.com>
2017-04-18 20:25:16 +0300

    Init HomeWork L3

---
+++ L3/src/main/java/com/otus/alexeenko/l3/L3.java
@@ -0,0 +1,28 @@
+package com.otus.alexeenko.l3;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class L3 {
+    private static final int MEASURE_COUNT = 100;
+
+    public static void main(String... args) {
+        Collection<Integer> example = new ArrayList<>();
+        int min = 0;
+        int max = 9_999_999;
+        for (int i = min; i < max + 1; i++) {
+            example.add(i);
+        }
+
+        findTheElement(() -> example.contains(max));
+    }
+
+    private static void findTheElement(Runnable runnable) {
+        long startTime = System.nanoTime();
+        for (int i = 0; i < MEASURE_COUNT; i++)
+            runnable.run();
+        long finishTime = System.nanoTime();
+        long timeNs = (finishTime - startTime) / MEASURE_COUNT;
+        System.out.println("Time spent: " + timeNs + "ns (" + timeNs / 1_000_000 + "ms)");
+    }
+}
