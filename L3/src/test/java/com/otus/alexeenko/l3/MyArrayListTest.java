4d6c9424e4f720a98789d93c71752710b9545634
Vsevolod <alexeenko.vsevolod@gmail.com>
2017-04-29 00:33:15 +0300

    L3 Release 3.7 (minor changes of the Test)

--- L3/src/test/java/com/otus/alexeenko/l3/MyArrayListTest.java
+++ L3/src/test/java/com/otus/alexeenko/l3/MyArrayListTest.java
@@ -1,83 +1,61 @@
 package com.otus.alexeenko.l3;
 
+import org.junit.Before;
 import org.junit.Test;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.Objects;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Created by Vsevolod on 19/04/2017.
  */
 public class MyArrayListTest {
     private static final int size = 9999;
-    
-    private final List<Integer> testList;
-    private final List<Integer> src;
-    private final List<Integer> dest;
 
-    public MyArrayListTest() {
-        testList = new ArrayList<>();
-        src = new MyArrayList<>();
-        dest = new MyArrayList<>();
+    private final List<Integer> src = new MyArrayList<>();
+    private final List<Integer> dest = new MyArrayList<>();
 
+    @Before
+    public void BeforeMyArrayListTest() {
         for (int i = 0; i < size; ++i) {
             src.add(i);
             dest.add(i);
-            testList.add(i);
         }
+
+        Collections.shuffle(dest);
+        Collections.shuffle(src);
     }
 
     @Test
     public void addAll() {
         Collections.addAll(src,101, 111, 100);
-        Collections.addAll(testList,101, 111, 100);
+        Collections.addAll(dest,101, 111, 100);
 
-        for (int i = 0; i < size + 3; ++i)
-            assertTrue(Objects.equals(src.get(i), testList.get(i)));
+        for (int i = size; i < size + 3; ++i)
+            assertEquals(src.get(i), dest.get(i));
     }
 
     @Test
     public void copy() {
-        List<Integer> anotherTestList = new ArrayList<>();
-        testList.clear();
-        
-        for (int i = 0; i < size; ++i) {
-            anotherTestList.add(i);
-            testList.add((int) (Math.random() * 100));
-        }
-        
-        Collections.copy(anotherTestList, testList);
-        Collections.copy(dest, testList);
-
+        Collections.copy(src, dest);
         for (int i = 0; i < size; ++i)
-            assertTrue(Objects.equals(dest.get(i), anotherTestList.get(i)));
+            assertEquals(dest.get(i), src.get(i));
 
-        Collections.shuffle(testList);
-        Collections.copy(testList, dest);
-
+        Collections.copy(dest, src);
         for (int i = 0; i < size; ++i)
-            assertTrue(Objects.equals(dest.get(i), dest.get(i)));
+            assertEquals(dest.get(i), src.get(i));
         
     }
 
+
     @Test
     public void sort() {
-        int temp;
-
-        for (int i = 0; i < size; ++i) {
-            temp = (int) (Math.random() * 100);
-            dest.add(temp);
-            testList.add(temp);
-        }
-
         Collections.sort(dest);
-        Collections.sort(testList);
+        Collections.sort(src);
 
         for (int i = 0; i < size; ++i)
-            assertTrue(Objects.equals(dest.get(i), testList.get(i)));
+            assertEquals(dest.get(i), src.get(i));
     }
 }
