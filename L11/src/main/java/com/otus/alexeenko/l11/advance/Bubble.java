e596378d5cab60e4e5a2a70ac363b43b5a1b0f23
Vsevolod <alexeenko.vsevolod@gmail.com>
2017-07-25 15:23:49 +0300

    L11 Release 3.4.1 (minor changes)

--- L11/src/main/java/com/otus/alexeenko/l11/advance/Bubble.java
+++ L11/src/main/java/com/otus/alexeenko/l11/advance/Bubble.java
@@ -1,7 +1,6 @@
 package com.otus.alexeenko.l11.advance;
 
 import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
