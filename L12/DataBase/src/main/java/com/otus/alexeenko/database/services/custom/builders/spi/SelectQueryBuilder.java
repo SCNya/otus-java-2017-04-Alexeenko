f94bc92a1a8976251abb2b69169d1b7135cb10e6
Vsevolod <alexeenko.vsevolod@gmail.com>
2017-08-23 23:12:17 +0300

    L12 Release 4.5.3 (refactored)

--- L12/DataBase/src/main/java/com/otus/alexeenko/database/services/custom/builders/spi/SelectQueryBuilder.java
+++ L12/DataBase/src/main/java/com/otus/alexeenko/database/services/custom/builders/spi/SelectQueryBuilder.java
@@ -1,10 +1,8 @@
 package com.otus.alexeenko.database.services.custom.builders.spi;
 
-import com.otus.alexeenko.database.services.custom.builders.internal.SelectBuilder;
-
 /**
  * Created by Vsevolod on 20/06/2017.
  */
 public interface SelectQueryBuilder extends QueryBuilder {
-    SelectBuilder addID(String columnName, long id);
+    SelectQueryBuilder addID(String columnName, long id);
 }
